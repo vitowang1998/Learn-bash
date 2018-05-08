1f455e25c5f5297547d3ee27f0a27af95d5fa57b
Haoran Wang <vitowang1998@gmail.com>
2018-05-08 17:14:32 -0400

    Adds a file about "chmod" command in bash.

---
+++ chmod.sh
@@ -0,0 +1,27 @@
+chmod
+
+
+
+chmod [mode] [file]
+-change mode
+-changes the permission of [file] to [mode]
+
+parties
+-u user
+-g group
+-o other
+-a all, everyone
+
+operators
++ add permission
+- remove permission
+= assign permission
+
+permission
+r read
+w write
+x execute
+
+chmod u+x [file]
+chmod g=wx [file]
+chmod o-r [file]
