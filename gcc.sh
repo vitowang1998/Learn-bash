ec5bfe7879a7cb4eeb37012d718fcaaf7ff8b82b
Haoran Wang <vitowang1998@gmail.com>
2018-05-08 17:42:57 -0400

    Adds a file about command "gcc" in bash.

---
+++ gcc.sh
@@ -0,0 +1,12 @@
+gcc
+
+
+
+gcc [file.c]
+-complile [file.c] with gcc compiler and name the output "a.out"
+
+gcc [file.c] -o b
+-compile [file.c] with gcc
+-name the output b
+-b can have any extension
+
