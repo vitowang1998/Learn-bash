89e6c87c7354c76ede2426769115caa05a052963
Haoran Wang <vitowang1998@gmail.com>
2018-05-08 17:15:36 -0400

    Adds a file about "wc" command in bash.

---
+++ wc.sh
@@ -0,0 +1,15 @@
+wc
+
+
+
+wc [file]
+print [count_lines count_words count_chars filename]
+
+wc -c [file]
+print the characters counts
+
+wc -l [file]
+print the newline counts
+
+wc -w [file]
+print the word counts
