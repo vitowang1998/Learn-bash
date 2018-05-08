3e41a40d847507691873edaf975b3e468b766fef
Haoran Wang <vitowang1998@gmail.com>
2018-05-08 17:42:17 -0400

    Adds a file about command "cat" in bash.

---
+++ cat.sh
@@ -0,0 +1,21 @@
+cat
+
+
+
+cat [file]
+-display the contents in a
+
+cat -n [file]
+-display the content of [file] and number the lines
+-cat can open multiple files at a time, redirect method cannot
+
+cat < [file]
+-redirect the content of [file] to cat. cat will display the content of [file]
+
+cat [src] > [dest]
+-equivalent as cp [src] [dest]
+-even if [src] does not exist, [dest] would also be created.
+
+cat [src] > [dest] 2> [err]
+-2> will redirect the error message
+-even if no errors generated, [err] would also be created.
