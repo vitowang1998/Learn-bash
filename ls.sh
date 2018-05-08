fd4e212af73bd97d3858e8d1d9848d684e083bfe
Haoran Wang <vitowang1998@gmail.com>
2018-05-08 16:49:05 -0400

    Adds a file about "ls" command in bash.

---
+++ ls.sh
@@ -0,0 +1,28 @@
+ls
+-This will list all files in current folder
+
+
+
+ls -a
+-Lists all files (include hidden files)
+-(Hidden files' name starts with a dot(.))
+
+
+
+ls -l
+-This will list all files along with
+
+
+
+ls *.txt
+-Wildcard matching
+-Lists all files of the form [whatever].txt
+
+
+
+ls -1
+-list all files in current folder.
+-every filename is in a single line
+
+
+
