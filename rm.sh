e2b0902b3ac6e409f77a019c19545c87ac10ca09
Haoran Wang <vitowang1998@gmail.com>
2018-05-09 23:55:42 -0400

    Adds a file about "rm" command in bash.

---
+++ rm.sh
@@ -0,0 +1,13 @@
+rm
+
+
+
+rm [file]
+-delete [file]
+-this will not permanently remove the file, it is ACTUALLY REMOVED when we reuse the memory
+-so it is technically possible to restore the files with special methods.
+
+
+rm *.txt
+-remove all files of the form [whatever].txt
+-it is clearly EXTREMELY DANGEROUS.
