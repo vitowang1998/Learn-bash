56b5660557cf7c8d70b699df61eab89db3fb7b7c
Haoran Wang <vitowang1998@gmail.com>
2018-05-12 21:16:19 -0400

    Adds a file about "rmdir" command in bash.

---
+++ rmdir.sh
@@ -0,0 +1,12 @@
+rmdir
+
+
+
+rmdir [folder_name]
+-remove an empty directory. An error occurs if attempt to remove a non-empty folder.
+-this is safer.
+
+
+rmdir -r [folder_name]
+-remove the contents [folder_name] and remove [folder_name]
+-can be dangerous since we're able to delete multiple files at once.
