1a41f12eaa60f43de7d8206ee1eb15cecc3d0036
Haoran Wang <vitowang1998@gmail.com>
2018-05-12 20:49:06 -0400

    Adds a file about "vim" in bash.

---
+++ vim.sh
@@ -0,0 +1,31 @@
+vim
+
+
+
+vim [file.extension]
+-open [file.extension] with vim.
+
+Exc
+-Return to Normal Mode
+
+h
+-left
+
+k
+-up
+
+j
+-down
+
+l
+-right
+
+
+u
+-undo (in Normal Mode)
+
+i
+-enter Insert Mode
+
+dd
+-delete a line
