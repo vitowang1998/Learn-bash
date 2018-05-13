9eafc5c98de8c73f67d32f1cf3bbfba8c3158924
Haoran Wang <vitowang1998@gmail.com>
2018-05-12 21:15:52 -0400

    Adds a file about permissions in bash.

---
+++ file_permissions.sh
@@ -0,0 +1,15 @@
+file permissions
+
+
+
+file permissions
+-1 2  3  4
+-drwxrwxrwx
+-1. file type (dir or document)
+-2. user permission
+-3. group permission
+-4. other permission
+
+r: read
+w: write
+x: execute
