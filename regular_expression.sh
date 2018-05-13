31a6c09b11c7d8d59eb693dedf6dccad840e51ce
Haoran Wang <vitowang1998@gmail.com>
2018-05-12 20:36:35 -0400

    Adds a file about regular expressions in bash.

---
+++ regular_expression.sh
@@ -0,0 +1,42 @@
+regular expression
+
+
+
+[0-9]
+-integers from 0 to 9
+
+[^a]
+-any character excepts a
+
+[a-z]
+-characters from a to z
+
+[A-Z]
+-characters from A to Z
+
+[AB]
+-A or B
+
+[AB|CD]
+-AB or CD
+
+.
+-matches any single character
+
+^
+-matches the beginning of a line
+
+$
+-matches the end of a line
+
++
+-1 or more occurrence of the proceeding expression
+
+?
+-0 or 1 occurrence of the proceeding expression
+
+
+
+
+
+
