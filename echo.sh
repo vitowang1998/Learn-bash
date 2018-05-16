90cfc20ddff3f63471492fe6d83bcedc5fd11dd9
Haoran Wang <vitowang1998@gmail.com>
2018-05-15 23:08:21 -0400

    Adds information for "echo" command in bash.

--- echo.sh
+++ echo.sh
@@ -6,3 +6,8 @@ echo $0
        [-bash] if we are using -bash
 
 
+
+echo [GOLBBING PARTTERN]
+-print the files in current directory that fits the [GOLBBING PARTTERN]
+-note that if there exists no files that fit the [GOLBBING PARTTERN], echo prints [GOLBBING PARTTERN]
+-hence we should not use echo to print the files that conform to a [GOLBBING PARTTERN], use "ls" instead.
