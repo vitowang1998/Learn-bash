c48318db69256f7f13ffb2fa18c719a2c9495152
Haoran Wang <vitowang1998@gmail.com>
2018-05-18 13:04:02 -0400

    Adds a file about bools in bash script.

---
+++ shell_script/bool.sh
@@ -0,0 +1,21 @@
+booleans
+
+
+
+[var1] -eq [var2]
+-true if var1 = var2
+
+[var1] -ne [var2]
+-true if var1 != var2
+
+[var1] -le [var2]
+-true if var1 <= var2
+
+[var1] -lt [var2]
+-true if var1 < var2
+
+[var1] -ge [var2]
+-true if var1 >= var2
+
+[var1] -gt [var2]
+-true if var1 > var2
