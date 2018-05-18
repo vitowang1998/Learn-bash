7b2f5d5be299f253690fabe9c9a1ee835731d2f4
Haoran Wang <vitowang1998@gmail.com>
2018-05-18 15:39:23 -0400

    Corrects and adds one more line for bool.sh

--- shell_script/bool.sh
+++ shell_script/bool.sh
@@ -2,20 +2,23 @@ booleans
 
 
 
-[var1] -eq [var2]
+[ var1 -eq var2 ]
 -true if var1 = var2
 
-[var1] -ne [var2]
+[ var1 -ne var2 ]
 -true if var1 != var2
 
-[var1] -le [var2]
+[ var1 -le var2 ]
 -true if var1 <= var2
 
-[var1] -lt [var2]
+[ var1 -lt var2 ]
 -true if var1 < var2
 
-[var1] -ge [var2]
+[ var1 -ge var2 ]
 -true if var1 >= var2
 
-[var1] -gt [var2]
+[ var1 -gt var2 ]
 -true if var1 > var2
+
+[ -e file_name ]
+-true if file_name exists
