4d78f8b36596f42cd39d84baf6314bc731750781
Haoran Wang <vitowang1998@gmail.com>
2018-06-04 23:20:11 -0400

    Adds a file about g++ command in bash.

---
+++ g++.sh
@@ -0,0 +1,10 @@
+g++
+
+g++ [file]
+-compile [file] with g++ compiler
+
+g++ [file] -o [name]
+-compile [file] with g++ and name the executable program [name]
+
+g++ [file] -std=c++14
+-compile [file] with g++ and use c++14 as the standard
