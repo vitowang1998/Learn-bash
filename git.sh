399a38793ff876b6797f7da4395a0c61ca46bb5a
Haoran Wang <vitowang1998@gmail.com>
2018-11-13 11:40:17 -0500

    Add git.sh

---
+++ git.sh
@@ -0,0 +1,22 @@
+git
+
+git branch
+-prints all the local branches
+
+git status
+-prints the current git status
+
+git clean -n
+-prints the files that would be removed
+
+git clean -f
+-remove the untracked files
+
+git clean -d -f
+-remove all untracked files and directories
+
+git stash
+-stores the current tracked work in a dirty directory
+
+git stash apply
+-restores the stashed work from the dirty directory
