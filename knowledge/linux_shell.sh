ac96ea8ba555b34654cc1cb77d61741358d5c93d
Haoran Wang <vitowang1998@gmail.com>
2018-11-14 13:37:35 -0500

    Add information for linux shell.

---
+++ knowledge/linux_shell.sh
@@ -0,0 +1,10 @@
+: '
+ shell: interface to the computer
+ graphical interface: easy to use, hard to do complex tasks
+ CLT: commend-line tool
+ Windows: DOS shell
+ UNIX: commend-line shell
+ Commend-line prompt: good for complex tasks, bad for discroverability and steep learning curve
+ First shell: Bourne shell -> Cshell -> Turbo shell (Descendent of Cshell) -> Bourne Again Shell (BASH)
+ echo $0: tells if the current shell is bash.
+'
